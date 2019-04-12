a0196e00197bcbca9331c11e8f02be93551ed8bb
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2019-04-12 09:35:17 +0200

    Vendors project with Go mod

---
+++ vendor/github.com/gig-tech/ovc-sdk-go/ovc/client.go
@@ -0,0 +1,137 @@
+package ovc
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Config used to connect to the API
+type Config struct {
+	Hostname     string
+	ClientID     string
+	ClientSecret string
+	JWT          string
+}
+
+// Credentials used to authenticate
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Client struct
+type Client struct {
+	JWT       string
+	ServerURL string
+	Access    string
+
+	Machines     MachineService
+	CloudSpaces  CloudSpaceService
+	Accounts     AccountService
+	Disks        DiskService
+	Portforwards ForwardingService
+	Templates    TemplateService
+	Sizes        SizesService
+	Images       ImageService
+}
+
+// Do sends and API Request and returns the body as an array of bytes
+func (c *Client) Do(req *http.Request) ([]byte, error) {
+	var body []byte
+	client := &http.Client{}
+	req.Header.Set("Authorization", "bearer "+c.JWT)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Status code: " + resp.Status)
+	log.Println("Body: " + string(body))
+	if resp.StatusCode > 202 {
+		return body, errors.New(string(body))
+	}
+
+	return body, nil
+}
+
+// NewClient returns a OpenVCloud API Client
+func NewClient(c *Config, url string) (*Client, error) {
+	if c.ClientID != "" && c.ClientSecret != "" && c.JWT != "" {
+		return nil, fmt.Errorf("ClientID, ClientSecret and JWT are provided, please only set ClientID and ClientSecret or JWT")
+	}
+
+	client := &Client{}
+	tokenString := ""
+	claims := jwt.MapClaims{}
+
+	if c.JWT == "" {
+		tokenString = NewLogin(c)
+	} else {
+		tokenString = c.JWT
+	}
+	jwt.ParseWithClaims(tokenString, claims, nil)
+	username, ok := claims["username"]
+	if !ok {
+		return nil, fmt.Errorf("JWT does not contain a username claim")
+	}
+
+	client.ServerURL = url + "/restmachine"
+	client.JWT = tokenString
+	client.Access = username.(string) + "@itsyouonline"
+
+	client.Machines = &MachineServiceOp{client: client}
+	client.CloudSpaces = &CloudSpaceServiceOp{client: client}
+	client.Accounts = &AccountServiceOp{client: client}
+	client.Disks = &DiskServiceOp{client: client}
+	client.Portforwards = &ForwardingServiceOp{client: client}
+	client.Templates = &TemplateServiceOp{client: client}
+	client.Sizes = &SizesServiceOp{client: client}
+	client.Images = &ImageServiceOp{client: client}
+
+	return client, nil
+}
+
+// NewLogin logs into the itsyouonline platform using the comfig struct
+func NewLogin(c *Config) string {
+
+	authForm := url.Values{}
+	authForm.Add("grant_type", "client_credentials")
+	authForm.Add("client_id", c.ClientID)
+	authForm.Add("client_secret", c.ClientSecret)
+	authForm.Add("response_type", "id_token")
+	req, _ := http.NewRequest("POST", "https://itsyou.online/v1/oauth/access_token", strings.NewReader(authForm.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error performing login request")
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body")
+	}
+	jwt := string(bodyBytes)
+	defer resp.Body.Close()
+	return jwt
+}
+
+// GetLocation parses the URL to return the location of the API
+func (c *Client) GetLocation() string {
+	u, _ := url.Parse(c.ServerURL)
+	hostName := u.Hostname()
+	return hostName[:strings.IndexByte(hostName, '.')]
+}
---
+++ vendor/github.com/gig-tech/ovc-sdk-go/ovc/machines.go
@@ -0,0 +1,324 @@
+package ovc
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+// MachineList is a list of machines
+// Returned when using the List method
+type MachineList []struct {
+	Status      string `json:"status"`
+	StackID     int    `json:"stackId"`
+	UpdateTime  int    `json:"updateTime"`
+	ReferenceID string `json:"referenceId"`
+	Name        string `json:"name"`
+	Nics        []struct {
+		Status      string `json:"status"`
+		MacAddress  string `json:"macAddress"`
+		ReferenceID string `json:"referenceId"`
+		DeviceName  string `json:"deviceName"`
+		Type        string `json:"type"`
+		Params      string `json:"params"`
+		NetworkID   int    `json:"networkId"`
+		GUID        string `json:"guid"`
+		IPAddress   string `json:"ipAddress"`
+	} `json:"nics"`
+	SizeID       int   `json:"sizeId"`
+	Disks        []int `json:"disks"`
+	CreationTime int   `json:"creationTime"`
+	ImageID      int   `json:"imageId"`
+	Storage      int   `json:"storage"`
+	Vcpus        int   `json:"vcpus"`
+	Memory       int   `json:"memory"`
+	ID           int   `json:"id"`
+}
+
+// MachineConfig is used when creating a machine
+type MachineConfig struct {
+	MachineID    string        `json:"machineId,omitempty"`
+	CloudspaceID int           `json:"cloudspaceId,omitempty"`
+	Name         string        `json:"name,omitempty"`
+	Description  string        `json:"description,omitempty"`
+	Memory       int           `json:"memory,omitempty"`
+	Vcpus        int           `json:"vcpus,omitempty"`
+	SizeID       int           `json:"sizeId,omitempty"`
+	ImageID      int           `json:"imageId,omitempty"`
+	Disksize     int           `json:"disksize,omitempty"`
+	DataDisks    []interface{} `json:"datadisks,omitempty"`
+	Permanently  bool          `json:"permanently,omitempty"`
+	Userdata     string        `json:"userdata,omitempty"`
+}
+
+// MachineInfo contains all information related to a cloudspace
+type MachineInfo struct {
+	Cloudspaceid int    `json:"cloudspaceid"`
+	Status       string `json:"status"`
+	UpdateTime   int    `json:"updateTime"`
+	Hostname     string `json:"hostname"`
+	Locked       bool   `json:"locked"`
+	Name         string `json:"name"`
+	CreationTime int    `json:"creationTime"`
+	Sizeid       int    `json:"sizeid"`
+	Disks        []struct {
+		Status  string `json:"status,omitempty"`
+		SizeMax int    `json:"sizeMax,omitempty"`
+		Name    string `json:"name,omitempty"`
+		Descr   string `json:"descr,omitempty"`
+		ACL     struct {
+		} `json:"acl"`
+		Type string `json:"type"`
+		ID   int    `json:"id"`
+	} `json:"disks"`
+	Storage int `json:"storage"`
+	ACL     []struct {
+		Status       string `json:"status"`
+		CanBeDeleted bool   `json:"canBeDeleted"`
+		Right        string `json:"right"`
+		Type         string `json:"type"`
+		UserGroupID  string `json:"userGroupId"`
+	} `json:"acl"`
+	OsImage  string `json:"osImage"`
+	Accounts []struct {
+		GUID     string `json:"guid"`
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	} `json:"accounts"`
+	Interfaces []struct {
+		Status      string `json:"status"`
+		MacAddress  string `json:"macAddress"`
+		ReferenceID string `json:"referenceId"`
+		DeviceName  string `json:"deviceName"`
+		IPAddress   string `json:"ipAddress"`
+		Params      string `json:"params"`
+		NetworkID   int    `json:"networkId"`
+		GUID        string `json:"guid"`
+		Type        string `json:"type"`
+	} `json:"interfaces"`
+	Imageid     int         `json:"imageid"`
+	ID          int         `json:"id"`
+	Memory      int         `json:"memory"`
+	Vcpus       int         `json:"vcpus"`
+	Description interface{} `json:"description"`
+}
+
+// MachineService is an interface for interfacing with the Machine
+// endpoints of the OVC API
+// See: https://ch-lug-dc01-001.gig.tech/g8vdc/#/ApiDocs
+//ht: added resize
+type MachineService interface {
+	List(int) (*MachineList, error)
+	Get(string) (*MachineInfo, error)
+	GetByName(string, string) (*MachineInfo, error)
+	Create(*MachineConfig) (string, error)
+	Update(*MachineConfig) (string, error)
+	Resize(*MachineConfig) (string, error)
+	Delete(*MachineConfig) error
+	DeleteByID(int) error
+	Template(int, string) error
+	Shutdown(int) error
+}
+
+// MachineServiceOp handles communication with the machine related methods of the
+// OVC API
+type MachineServiceOp struct {
+	client *Client
+}
+
+var _ MachineService = &MachineServiceOp{}
+
+// List all machines
+func (s *MachineServiceOp) List(cloudSpaceID int) (*MachineList, error) {
+	cloudSpaceIDMap := make(map[string]interface{})
+	cloudSpaceIDMap["cloudspaceId"] = cloudSpaceID
+	cloudSpaceIDJSON, err := json.Marshal(cloudSpaceIDMap)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/list", bytes.NewBuffer(cloudSpaceIDJSON))
+	if err != nil {
+		return nil, err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var machines = new(MachineList)
+	err = json.Unmarshal(body, &machines)
+	if err != nil {
+		return nil, err
+	}
+	return machines, nil
+}
+
+// Get individual machine
+func (s *MachineServiceOp) Get(id string) (*MachineInfo, error) {
+	machineIDMap := make(map[string]interface{})
+	machineIDMap["machineId"], _ = strconv.Atoi(id)
+	machineIDJson, err := json.Marshal(machineIDMap)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/get", bytes.NewBuffer(machineIDJson))
+	if err != nil {
+		return nil, err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var machineInfo = new(MachineInfo)
+	err = json.Unmarshal(body, &machineInfo)
+	if err != nil {
+		return nil, err
+	}
+	return machineInfo, nil
+}
+
+// GetByName gets an individual machine from its name
+func (s *MachineServiceOp) GetByName(name string, cloudspaceID string) (*MachineInfo, error) {
+	cid, err := strconv.Atoi(cloudspaceID)
+	if err != nil {
+		return nil, err
+	}
+	machines, err := s.client.Machines.List(cid)
+	if err != nil {
+		return nil, err
+	}
+	for _, mc := range *machines {
+		if mc.Name == name {
+			return s.client.Machines.Get(strconv.Itoa(mc.ID))
+		}
+	}
+	return nil, nil
+}
+
+// Create a new machine
+func (s *MachineServiceOp) Create(machineConfig *MachineConfig) (string, error) {
+	machineJSON, err := json.Marshal(*machineConfig)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/create", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return "", err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+// Update an existing machine
+func (s *MachineServiceOp) Update(machineConfig *MachineConfig) (string, error) {
+	machineJSON, err := json.Marshal(*machineConfig)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/update", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return "", err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+//ht: reszize an existing machine
+func (s *MachineServiceOp) Resize(machineConfig *MachineConfig) (string, error) {
+	machineJSON, err := json.Marshal(*machineConfig)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/resize", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return "", err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+// Delete an existing machine
+func (s *MachineServiceOp) Delete(machineConfig *MachineConfig) error {
+	machineJSON, err := json.Marshal(*machineConfig)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/delete", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteByID deletes an existing machine by ID
+func (s *MachineServiceOp) DeleteByID(machineID int) error {
+	machineMap := make(map[string]interface{})
+	machineMap["machineId"] = machineID
+	machineMap["permanently"] = true
+	machineJSON, err := json.Marshal(machineMap)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/delete", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Template creates an image of the existing machine by ID
+func (s *MachineServiceOp) Template(machineID int, templateName string) error {
+	machineMap := make(map[string]interface{})
+	machineMap["machineId"] = machineID
+	machineMap["templateName"] = templateName
+	machineJSON, err := json.Marshal(machineMap)
+	if err != nil {
+		return nil
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/createTemplate", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Shutdown shuts a machine down
+func (s *MachineServiceOp) Shutdown(machineID int) error {
+	machineMap := make(map[string]interface{})
+	machineMap["machineId"] = machineID
+	machineMap["force"] = false
+	machineJSON, err := json.Marshal(machineMap)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/machines/stop", bytes.NewBuffer(machineJSON))
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}

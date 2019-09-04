42dd61a1d544b8c13d6fd6054eb21ee48a2c36ec
Ekaterina Evdokimova <ekaterina.evdokimova@gig.tech>
2019-09-04 16:40:25 +0200

    vendoring

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/gig-tech/terraform-provider-ovc
 go 1.11
 
 require (
-	github.com/gig-tech/ovc-sdk-go v1.4.0
+	github.com/gig-tech/ovc-sdk-go v1.4.1-0.20190904143426-a57250c5d38b
 	github.com/hashicorp/terraform v0.11.13
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20190904005037-43c01164e931 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- vendor/github.com/gig-tech/ovc-sdk-go/ovc/client.go
+++ vendor/github.com/gig-tech/ovc-sdk-go/ovc/client.go
@@ -1,12 +1,15 @@
 package ovc
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -116,8 +119,40 @@ func NewClient(c *Config) (*Client, error) {
 	return client, nil
 }
 
+// async adds "async=true" flag to all API calls
+func async(req *http.Request) (*http.Request, error) {
+	// fetch request body to the string
+	jsonMap := make(map[string]interface{})
+
+	if req.Body != nil {
+		reqBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(reqBody, &jsonMap)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	jsonMap["_async"] = true
+	configJSON, err := json.Marshal(jsonMap)
+	if err != nil {
+		return nil, err
+	}
+	newReq, err := http.NewRequest(req.Method, req.URL.String(), bytes.NewBuffer(configJSON))
+	if err != nil {
+		return nil, err
+	}
+	return newReq, nil
+}
+
 // Do sends and API Request and returns the body as an array of bytes
 func (c *Client) Do(req *http.Request) ([]byte, error) {
+	req, err := async(req) // make request asynchronous
+	if err != nil {
+		return nil, err
+	}
 	client := &http.Client{}
 	tokenString, err := c.JWT.Get()
 	if err != nil {
@@ -137,19 +172,95 @@ func (c *Client) Do(req *http.Request) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	taskID := string(body)
 
 	c.logger.Debug("OVC call: " + req.URL.Path)
 	c.logger.Debug("OVC response status code: " + resp.Status)
-	c.logger.Debug("OVC response body: " + string(body))
+	c.logger.Debug("OVC response body: " + string(taskID))
 
 	switch {
 	case resp.StatusCode == 401:
 		return nil, ErrAuthentication
 	case resp.StatusCode > 202:
-		return body, errors.New(string(body))
+		return body, errors.New(taskID)
 	}
 
-	return body, nil
+	// remove quotes from taskID if contains any
+	taskID = strings.Replace(taskID, "\"", "", -1)
+
+	// create request to get result of an async API call by job id
+	taskJSON, err := json.Marshal(
+		struct {
+			TaskID string `json:"taskguid"`
+		}{
+			TaskID: taskID,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var taskResp *http.Response
+	result := make([]interface{}, 0)
+	start, timeout := time.Now(), 10*time.Minute
+
+	// wait for result of the async task
+	for {
+		taskReq, err := http.NewRequest("POST", c.ServerURL+"/system/task/get", bytes.NewBuffer(taskJSON))
+		if err != nil {
+			return nil, err
+		}
+		taskReq.Header.Set("Authorization", fmt.Sprintf("bearer %s", tokenString))
+		taskReq.Header.Set("Content-Type", "application/json")
+		taskResp, err = client.Do(taskReq)
+		if taskResp != nil {
+			defer taskResp.Body.Close()
+		}
+		if err != nil {
+			return nil, err
+		}
+		switch {
+		case taskResp.StatusCode == 401:
+			return nil, ErrAuthentication
+		case taskResp.StatusCode == 404:
+			// task may have not been registered yet
+			continue
+		case taskResp.StatusCode > 202:
+			return nil, errors.New(taskID)
+		}
+		resultBody, err := ioutil.ReadAll(taskResp.Body)
+		if err != nil {
+			return nil, err
+		}
+		if len(resultBody) != 0 {
+			// if body is not empty, parse result
+			err = json.Unmarshal(resultBody, &result)
+			if err != nil {
+				return resultBody, err
+			}
+			if len(result) != 0 {
+				// result is not empty if can be parsed to a []interface{}
+				break
+			}
+		}
+		if now := time.Now(); now.Sub(start) > timeout {
+			return nil, fmt.Errorf("job timeout %s", taskID)
+		}
+		time.Sleep(2 * time.Second)
+	}
+
+	success, ok := result[0].(bool)
+	if !ok {
+		return nil, fmt.Errorf("Task response is incorrect taskId %v \n expected response in form [True/False, taskResult], received: \n %v", string(taskID), result)
+	}
+	if !success {
+		return nil, fmt.Errorf("Task was not successfull taskID: %v:\n %v", string(taskID), result[1])
+	}
+	finalBody, err := json.Marshal(result[1])
+	if err != nil {
+		return finalBody, err
+	}
+	return finalBody, nil
 }
 
 // GetLocation parses the URL to return the location of the API
